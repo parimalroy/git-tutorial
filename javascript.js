fa11228f046452c0fd02f7a503fb05aa28f17785
parimalroy <parimal.tbd@gmail.com>
2020-07-18 15:24:37 +0600

    New Fuction fullName add to javascript file

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 
 greating('parimal')
+fullName('parimal','kumar','roy')
 
 function greating(name) {
     console.log("Hello "+ name)
+}
+
+
+function fullName(firstName,middleName,lastName) {
+    console.log('HI '+firstName+' '+middleName+' '+lastName)
 }
